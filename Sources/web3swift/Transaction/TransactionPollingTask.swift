5ac435512cba738a60cdb2faaea5141aa62a7bb8
ngn999 <ngn998@gmail.com>
2023-10-08 11:10:32 +0800

    update TransactionPollingTask sleep timeinterval

--- Sources/web3swift/Transaction/TransactionPollingTask.swift
+++ Sources/web3swift/Transaction/TransactionPollingTask.swift
@@ -11,9 +11,9 @@ import Web3Core
 final public class TransactionPollingTask {
 
     private enum DelayUnit: UInt64 {
-        case shortest = 1
-        case medium = 5
-        case longest = 60
+        case shortest = 1_000_000_000
+        case medium = 1_500_000_000
+        case longest = 2_000_000_000
 
         func shouldIncreaseDelay(_ startTime: Date) -> Bool {
             let timePassed = Date().timeIntervalSince1970 - startTime.timeIntervalSince1970
